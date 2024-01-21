a93c7202f4e64291d1b318aa6ac51f2efc222356
Williannoterdan <136361912+Williannoterdan@users.noreply.github.com>
2024-01-21 03:37:58 +0200

    fix

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,12 +1,11 @@
+let baseUser = { email: '', password: '' };
 console.log('dfd');
 
 const form = document.querySelector('.feedback-form');
 const localStorageKeyEmail = 'feedback-form-state-email';
 const localStorageKeysMessage = 'feedback-form-state-message';
-// form.elements.message.value = localStorage.getItem(localStorageKey) ?? '';
 form.elements.email.value = localStorage.getItem(localStorageKeyEmail) ?? '';
-form.elements.message.value =
-  localStorage.getItem(localStorageKeysMessage) ?? '';
+form.elements.message.value =localStorage.getItem(localStorageKeysMessage) ?? '';
 
 form.elements.email.addEventListener('input', evt => {
   localStorage.setItem(localStorageKeyEmail, evt.target.value);
@@ -14,10 +13,16 @@ form.elements.email.addEventListener('input', evt => {
 form.elements.message.addEventListener('input', evt => {
   localStorage.setItem(localStorageKeysMessage, evt.target.value);
 });
-form.addEventListener('submit', evt => {
-  evt.preventDefault();
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  let emailos = event.target.email.value;
+  let passwordos = event.target.message.value;
+  baseUser = {
+    email: emailos,
+    password: passwordos,
+  };
+  console.log(baseUser);
   localStorage.removeItem(localStorageKeyEmail);
   localStorage.removeItem(localStorageKeysMessage);
-
   form.reset();
 });
